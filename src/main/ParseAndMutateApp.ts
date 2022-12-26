5f77d3f7ab31f547eb69473ce51dc85a546b216c
Peter Cao <ppc49@cornell.edu>
2022-12-26 01:19:40 -0800

    file printing

--- src/main/ParseAndMutateApp.ts
+++ src/main/ParseAndMutateApp.ts
@@ -5,6 +5,11 @@ const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
+const fs = require('fs');
+const readline = require('readline');
+const events = require('events');
+
+
 
 clear();
 // console.log(
@@ -22,5 +27,19 @@ program
 
 var options = program.opts();
 
+var filepath = program.args[0];
+
+const allFileContents = fs.readFileSync(filepath, 'utf-8');
+
+allFileContents.split(/\r?\n/).forEach((line: string) => {
+  console.log(`Line from file: ${line}`);
+});
+console.log('');
+
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+console.log('')
+
 console.log('num mutations: %j', options.mutate);
-console.log('filename: %j', program.args[0]);
+console.log('filename: %j', filepath);
